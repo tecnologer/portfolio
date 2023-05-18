c55ef2911b9458ceeee7e2d705cec3d66e5b8e7d
Rey David Dominguez Soto <rdominguez@tecnologer.net>
2023-05-17 20:24:02 -0600

    Formatter (#2)
    * add chroma
    
    * add favicon
    
    * implement chroma

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"regexp"
 	"runtime"
 	"time"
+
+	"github.com/alecthomas/chroma/quick"
 )
 
 var (
@@ -28,83 +30,27 @@ var (
 	}{
 		{
 			symbols: []string{
-				`"([^"]+)"`,
-				"`([^`]+)`",
+				`(<span class="nx">Experience<\/span><span class="p">:<\/span>\s*<span class="nf">)(ListExperience)(<\/span>)`,
 			},
-			format: `<span class="string">$1</span>`,
+			format: `$1<a href="experience.html" title="Expand experience list" class="nf">$2</a>$3`,
 		},
 		{
 			symbols: []string{
-				`[^"']string`,
+				`(<span class="nx">ContactOptions<\/span><span class="p">:<\/span>\s*<span class="nf">)(ListContactOptions)(<\/span>)`,
 			},
-			format: `<span class="primitivetype">${1}</span>`,
-		},
-		{
-			symbols: []string{
-				" nil",
-				"package ",
-				"type ",
-				" struct ",
-				"func ",
-				"return ",
-				"\\[",
-				"\\]",
-			},
-			format: `<span class="keyword">$1</span>`,
-		},
-		{
-			symbols: []string{
-				"ListExperience",
-			},
-			format: `<a href="experience.html" title="Expand experience list">$1</a>`,
-		},
-		{
-			symbols: []string{
-				"\\(",
-				"\\)",
-			},
-			format: `<span class="parenthesis">$1</span>`,
-		},
-		{
-			symbols: []string{
-				"ListContactOptions",
-			},
-			format: `<a href="contact.html" title="See contact options">$1</a>`,
-		},
-		{
-			symbols: []string{
-				"\\{",
-				"\\}",
-			},
-			format: `<span class="curlybrace">$1</span>`,
-		},
-		{
-			symbols: []string{
-				"NewMe",
-				"ListExperience",
-				"ListContactOptions",
-			},
-			format: `<span class="function">$1</span>`,
-		},
-		{
-			symbols: []string{
-				"^//.*",
-				`^/\*.*\*/`,
-				"[^https:]//.*",
-			},
-			format: `<span class="comment">$1</span>`,
+			format: `$1<a href="contact.html" title="See contact options" class="nf">$2</a>$3`,
 		},
 		{
 			symbols: []string{
 				"rdominguez@tecnologer.net",
 			},
-			format: `<a href="mailto:${1}">${1}</a>`,
+			format: `<a href="mailto:${1}" class="s">${1}</a>`,
 		},
 		{
 			symbols: []string{
 				`"(http(s)?:\/\/.+)"`,
 			},
-			format: `<a href="${2}">${1}</a>`,
+			format: `<a href="${2}" class="s">${1}</a>`,
 		},
 	}
 )
@@ -144,7 +90,7 @@ func main() {
 	rules = map[string][]byte{
 		"{{code}}":    formatText(fileExp),
 		"{{file}}":    []byte("experience"),
-		"{{go_back}}": []byte(`<a href="index.html" title="return to home"><</a>`),
+		"{{go_back}}": homeIcon(),
 		"{{version}}": []byte(version),
 	}
 
@@ -153,7 +99,7 @@ func main() {
 	rules = map[string][]byte{
 		"{{code}}":    formatText(fileContact),
 		"{{file}}":    []byte("contact"),
-		"{{go_back}}": []byte(`<a href="index.html" title="return to home"><</a>`),
+		"{{go_back}}": homeIcon(),
 		"{{version}}": []byte(version),
 	}
 
@@ -162,12 +108,21 @@ func main() {
 }
 
 func formatText(fileContent []byte) []byte {
+	var buf bytes.Buffer
+
+	err := quick.Highlight(&buf, string(fileContent), "Go", "html", "dracula")
+	if err != nil {
+		log.Println(err)
+		return fileContent
+	}
+
+	fileContent = buf.Bytes()
+
 	var reg *regexp.Regexp
-	var err error
 
 	for _, f := range format {
 		for _, word := range f.symbols {
-			reg, err = regexp.Compile(fmt.Sprintf("(?mi)(%s)", word))
+			reg, err = regexp.Compile(fmt.Sprintf("(?mi)%s", word))
 			if err != nil {
 				log.Println(err)
 				continue
@@ -199,4 +154,12 @@ func getDir() (string, error) {
 	}
 
 	return path.Dir(filename), nil
+}
+
+func homeIcon() []byte {
+	return []byte(`<a href="index.html" class="icon-link" title="return to home">
+		<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 -5 50 50">
+    <path d="M 25 1.0507812 C 24.7825 1.0507812 24.565859 1.1197656 24.380859 1.2597656 L 1.3808594 19.210938 C 0.95085938 19.550938 0.8709375 20.179141 1.2109375 20.619141 C 1.5509375 21.049141 2.1791406 21.129062 2.6191406 20.789062 L 4 19.710938 L 4 46 C 4 46.55 4.45 47 5 47 L 19 47 L 19 29 L 31 29 L 31 47 L 45 47 C 45.55 47 46 46.55 46 46 L 46 19.710938 L 47.380859 20.789062 C 47.570859 20.929063 47.78 21 48 21 C 48.3 21 48.589063 20.869141 48.789062 20.619141 C 49.129063 20.179141 49.049141 19.550938 48.619141 19.210938 L 25.619141 1.2597656 C 25.434141 1.1197656 25.2175 1.0507812 25 1.0507812 z M 35 5 L 35 6.0507812 L 41 10.730469 L 41 5 L 35 5 z"></path>
+</svg>
+	</a>`)
 }
